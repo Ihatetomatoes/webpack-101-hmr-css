10d7a878bc48bdb8c757b68431810baa21f065de
Ihatetomatoes <peter@ihatetomatoes.net>
2017-03-01 11:41:01 +1100

    Final files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Webpack 2 - Hot Module Replacement for CSS
+
+Learn how to setup Webpack 2 and hot reload your CSS or SASS.
+
+## Webpack 2 Playlist
+
+[Watch on YouTube](https://www.youtube.com/watch?v=JdGnYNtuEtE&list=PLkEZWD8wbltnRp6nRR8kv97RbpcUdNawY)
+
+## How to use the files?
+
+1. Clone this repo
+2. Run `npm install` to install all the dependencies
+3. Run `npm run dev` for development mode
+4. New browser window should open automatically.
+
+Don't forget to [subscribe to my channel](https://www.youtube.com/channel/UC7O6CntQoAI-wYyJxYiqNUg?sub_confirmation=1) for more front-end videos.
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var webpack = require('webpack');
+var path = require("path");
+
+var isProd = process.env.NODE_ENV === 'production'; //true or false
+var cssDev = ['style-loader', 'css-loader', 'sass-loader'];
+var cssProd = ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    loader: ['css-loader','sass-loader'],
+    publicPath: '/dist'
+})
+var cssConfig = isProd ? cssProd : cssDev;
+
+
+module.exports = {
+    entry: {
+        app: './src/app.js'
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/, 
+                use: cssConfig
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.pug$/,
+                use: 'pug-html-loader'
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        hot: true,
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Project Demo',
+            hash: true,
+            template: './src/index.pug'
+        }),
+        new ExtractTextPlugin({
+            filename: 'app.css',
+            disable: !isProd,
+            allChunks: true
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
+    ]
+}
